bbbded405d259892229bd3fd9b2136bd9c34b8b3
danbleile <danbleile@gmail.com>
2020-01-07 11:06:29 -0800

    Build test template

--- README.md
+++ README.md
@@ -1,1 +1,167 @@
-# wsuwp-vvv-network-template
+# VVV Custom site template
+
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/6fc9d45abb02454aa052771bda2d40ff)](https://www.codacy.com/gh/Varying-Vagrant-Vagrants/custom-site-template?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=Varying-Vagrant-Vagrants/custom-site-template&amp;utm_campaign=Badge_Grade)
+
+For when you just need a simple dev site
+
+## Overview
+
+This template will allow you to create a WordPress dev environment using only `vvv-custom.yml`.
+
+The supported environments are:
+
+- A single site
+- A subdomain multisite
+- A subdirectory multisite
+
+The Nginx configuration for this site can be overriden by creating a `vvv-nginx-custom.conf`.
+
+## Configuration
+
+### The Minimum Required Configuration
+
+```yaml
+my-site:
+  repo: https://github.com/Varying-Vagrant-Vagrants/custom-site-template
+  hosts:
+    - my-site.test
+```
+
+| Setting    | Value        |
+|------------|--------------|
+| Domain     | my-site.test |
+| Site Title | my-site.test |
+| DB Name    | my-site      |
+| Site Type  | Single       |
+| WP Version | Latest       |
+
+### Minimal configuration with custom domain and WordPress Nightly
+
+```yaml
+my-site:
+  repo: https://github.com/Varying-Vagrant-Vagrants/custom-site-template
+  hosts:
+    - foo.test
+  custom:
+    wp_version: nightly
+```
+
+| Setting    | Value       |
+|------------|-------------|
+| Domain     | foo.test    |
+| Site Title | foo.test    |
+| DB Name    | my-site     |
+| Site Type  | Single      |
+| WP Version | Nightly     |
+
+### WordPress Multisite with Subdomains
+
+```yaml
+my-site:
+  repo: https://github.com/Varying-Vagrant-Vagrants/custom-site-template
+  hosts:
+    - multisite.test
+    - site1.multisite.test
+    - site2.multisite.test
+  custom:
+    wp_type: subdomain
+
+```
+| Setting    | Value               |
+|------------|---------------------|
+| Domain     | multisite.test      |
+| Site Title | multisite.test      |
+| DB Name    | my-site             |
+| Site Type  | Subdomain Multisite |
+
+### WordPress Multisite with Subdirectory
+
+```yaml
+my-site:
+  repo: https://github.com/Varying-Vagrant-Vagrants/custom-site-template
+  hosts:
+    - multisite.test
+  custom:
+    wp_type: subdirectory
+```
+
+| Setting    | Value                  |
+|------------|------------------------|
+| Domain     | multisite.test         |
+| Site Title | multisite.test         |
+| DB Name    | my-site                |
+| Site Type  | Subdirectory Multisite |
+
+## Configuration Options
+
+```yaml
+hosts:
+    - foo.test
+    - bar.test
+    - baz.test
+```
+
+Defines the domains and hosts for VVV to listen on.
+The first domain in this list is your sites primary domain.
+
+```yaml
+custom:
+    site_title: My Awesome Dev Site
+```
+
+Defines the site title to be set upon installing WordPress.
+
+```yaml
+custom:
+    wp_version: 4.6.4
+```
+
+Defines the WordPress version you wish to install. Valid values are:
+
+- nightly
+- latest
+- a version number
+
+Older versions of WordPress will not run on PHP7, see this page on [how to change PHP version per site](https://varyingvagrantvagrants.org/docs/en-US/adding-a-new-site/changing-php-version/).
+
+```yaml
+custom:
+    wp_type: single
+```
+Defines the type of install you are creating. Valid values are:
+
+- single
+- subdomain
+- subdirectory
+- none
+
+```yaml
+custom:
+    db_name: super_secet_db_name
+```
+
+Defines the DB name for the installation.
+
+Other parameters available:
+
+```yaml
+custom:
+    delete_default_plugins: true # Only on install of WordPress
+    install_test_content: true # Install test content. Only on install of WordPress
+    install_plugins: # Various way to install a plugin
+         - query-monitor
+         - https://github.com/crstauf/query-monitor-extend/archive/version/1.0.zip
+         - https://github.com/norcross/airplane-mode/archive/master.zip
+    install_themes: # Various way to install a theme
+         - understrap
+         - https://github.com/understrap/understrap-child.git
+    wpconfig_constants:
+         WP_DEBUG: true
+         WP_DEBUG_LOG: true
+         WP_DISABLE_FATAL_ERROR_HANDLER: true # To disable in WP 5.2 the FER mode
+    locale: it_IT
+    admin_user: admin # Only on install of WordPress
+    admin_password: password # Only on install of WordPress
+    admin_email: admin@local.test # Only on install of WordPress
+    live_url: http://example.com # Redirect any uploads not found locally to this domain
+```
---
+++ provision/vvv-init.sh
@@ -0,0 +1,164 @@
+#!/usr/bin/env bash
+# Provision WordPress Stable
+
+set -eo pipefail
+
+echo " * Custom site template provisioner ${VVV_SITE_NAME} - downloads and installs a copy of WP stable for testing, building client sites, etc"
+
+# fetch the first host as the primary domain. If none is available, generate a default using the site name
+DOMAIN=$(get_primary_host "${VVV_SITE_NAME}".test)
+SITE_TITLE=$(get_config_value 'site_title' "${DOMAIN}")
+WP_VERSION=$(get_config_value 'wp_version' 'latest')
+WP_LOCALE=$(get_config_value 'locale' 'en_US')
+WP_TYPE=$(get_config_value 'wp_type' "single")
+DB_NAME=$(get_config_value 'db_name' "${VVV_SITE_NAME}")
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*]/}
+
+# Make a database, if we don't already have one
+echo -e " * Creating database '${DB_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`"
+echo -e " * Granting the wp user priviledges to the '${DB_NAME}' database"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e " * DB operations done."
+
+
+echo " * Setting up the log subfolder for Nginx logs"
+noroot mkdir -p "${VVV_PATH_TO_SITE}/log"
+noroot touch "${VVV_PATH_TO_SITE}/log/nginx-error.log"
+noroot touch "${VVV_PATH_TO_SITE}/log/nginx-access.log"
+
+echo " * Creating public_html folder if it doesn't exist already"
+noroot mkdir -p "${VVV_PATH_TO_SITE}/public_html"
+
+cd "${VVV_PATH_TO_SITE}/public_html"
+
+git clone https://github.com/wsuwebteam/wsuwp-platform-legacy.git
+
+if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
+    echo " * Setting up wp-config.php"
+    noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp  --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'SCRIPT_DEBUG', true );
+PHP
+  fi
+
+if [ "${WP_TYPE}" != "none" ]; then
+
+  # Install and configure the latest stable version of WordPress
+  if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
+    echo " * Downloading WordPress version '${WP_VERSION}' locale: '${WP_LOCALE}'"
+    noroot wp core download --locale="${WP_LOCALE}" --version="${WP_VERSION}"
+  fi
+
+  if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
+    echo " * Setting up wp-config.php"
+    noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp  --extra-php <<PHP
+define( 'WP_DEBUG', true );
+define( 'SCRIPT_DEBUG', true );
+PHP
+  fi
+
+  if ! $(noroot wp core is-installed ); then
+    echo " * WordPress is present but isn't installed to the database, checking for SQL dumps in wp-content/database.sql or the main backup folder."
+    if [ -f "${VVV_PATH_TO_SITE}/public_html/wp-content/database.sql" ]; then
+      echo " * Found a database backup at public_html/wp-content/database.sql. Restoring the site"
+      noroot wp config set DB_USER "wp"
+      noroot wp config set DB_PASSWORD "wp"
+      noroot wp config set DB_HOST "localhost"
+      noroot wp config set DB_NAME "${DB_NAME}"
+      noroot wp db import "${VVV_PATH_TO_SITE}/public_html/wp-content/database.sql"
+      echo " * Installed database backup"
+    elif [ -f "/srv/database/backups/${VVV_SITE_NAME}.sql" ]; then
+      echo " * Found database backup at /srv/database/backups/${VVV_SITE_NAME}.sql, restoring the site"
+      noroot wp config set DB_USER "wp"
+      noroot wp config set DB_PASSWORD "wp"
+      noroot wp config set DB_HOST "localhost"
+      noroot wp config set DB_NAME "${DB_NAME}"
+      noroot wp db import "/srv/database/backups/${VVV_SITE_NAME}.sql"
+      echo " * Installed database backup"
+    else
+      echo " * Installing WordPress Stable"
+
+      ADMIN_USER=$(get_config_value 'admin_user' "admin")
+      ADMIN_PASSWORD=$(get_config_value 'admin_password' "password")
+      ADMIN_EMAIL=$(get_config_value 'admin_email' "admin@local.test")
+
+      echo " * Installing using wp core install --url=\"${DOMAIN}\" --title=\"${SITE_TITLE}\" --admin_name=\"${ADMIN_USER}\" --admin_email=\"${ADMIN_EMAIL}\" --admin_password=\"${ADMIN_PASSWORD}\" --path=\"${VVV_PATH_TO_SITE}/public_html\""
+      noroot wp core install --url="${DOMAIN}" --title="${SITE_TITLE}" --admin_name="${ADMIN_USER}" --admin_email="${ADMIN_EMAIL}" --admin_password="${ADMIN_PASSWORD}"
+      echo " * WordPress was installed, with the username '${ADMIN_USER}', and the password '${ADMIN_PASSWORD}' at '${ADMIN_EMAIL}'"
+
+      if [ "${WP_TYPE}" = "subdomain" ]; then
+        echo " * Running Multisite install using wp core multisite-install --subdomains --url=\"${DOMAIN}\" --title=\"${SITE_TITLE}\" --admin_name=\"${ADMIN_USER}\" --admin_email=\"${ADMIN_EMAIL}\" --admin_password=\"${ADMIN_PASSWORD}\" --path=\"${VVV_PATH_TO_SITE}/public_html\""
+        noroot wp core multisite-install --subdomains --url="${DOMAIN}" --title="${SITE_TITLE}" --admin_name="${ADMIN_USER}" --admin_email="${ADMIN_EMAIL}" --admin_password="${ADMIN_PASSWORD}"
+        echo " * Multisite install complete"
+      elif [ "${WP_TYPE}" = "subdirectory" ]; then
+        echo " * Running Multisite install using wp core ${INSTALL_COMMAND} --url=\"${DOMAIN}\" --title=\"${SITE_TITLE}\" --admin_name=\"${ADMIN_USER}\" --admin_email=\"${ADMIN_EMAIL}\" --admin_password=\"${ADMIN_PASSWORD}\" --path=\"${VVV_PATH_TO_SITE}/public_html\""
+        noroot wp core multisite-install --url="${DOMAIN}" --title="${SITE_TITLE}" --admin_name="${ADMIN_USER}" --admin_email="${ADMIN_EMAIL}" --admin_password="${ADMIN_PASSWORD}"
+        echo " * Multisite install complete"
+      fi
+    fi
+  else
+    # to-do: Check if everything is up-to-date
+  fi
+else
+  echo " * wp_type was set to none, provisioning WP was skipped, moving to Nginx configs"
+fi
+
+echo " * Copying the sites Nginx config template"
+if [ -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" ]; then
+  echo " * A vvv-nginx-custom.conf file was found"
+  cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+  echo " * Using the default vvv-nginx-default.conf, to customize, create a vvv-nginx-custom.conf"
+  cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-default.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
+
+LIVE_URL=$(get_config_value 'live_url' '')
+if [ ! -z "$LIVE_URL" ]; then
+  echo " * Adding support for Live URL redirects to NGINX of the website's media"
+  # replace potential protocols, and remove trailing slashes
+  LIVE_URL=$(echo "${LIVE_URL}" | sed 's|https://||' | sed 's|http://||'  | sed 's:/*$::')
+
+  redirect_config=$((cat <<END_HEREDOC
+if (!-e \$request_filename) {
+  rewrite ^/[_0-9a-zA-Z-]+(/wp-content/uploads/.*) \$1;
+}
+if (!-e \$request_filename) {
+  rewrite ^/wp-content/uploads/(.*)\$ \$scheme://${LIVE_URL}/wp-content/uploads/\$1 redirect;
+}
+END_HEREDOC
+
+  ) |
+  # pipe and escape new lines of the HEREDOC for usage in sed
+  sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n\\1/g'
+  )
+
+  sed -i -e "s|\(.*\){{LIVE_URL}}|\1${redirect_config}|" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+  sed -i "s#{{LIVE_URL}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
+
+get_config_value 'wpconfig_constants' |
+  while IFS='' read -r -d '' key &&
+        IFS='' read -r -d '' value; do
+      echo " * Adding constant '${key}' with value '${value}' to wp-config.php"
+      noroot wp config set "${key}" "${value}" --raw
+  done
+
+WP_PLUGINS=$(get_config_value 'install_plugins' '')
+if [ ! -z "${WP_PLUGINS}" ]; then
+  for plugin in ${WP_PLUGINS//- /$'\n'}; do
+      echo " * Installing/activating plugin: '${plugin}'"
+      noroot wp plugin install "${plugin}" --activate
+  done
+fi
+
+WP_THEMES=$(get_config_value 'install_themes' '')
+if [ ! -z "${WP_THEMES}" ]; then
+    for theme in ${WP_THEMES//- /$'\n'}; do
+      echo " * Installing theme: '${theme}'"
+      noroot wp theme install "${theme}"
+    done
+fi
+
+echo " * Site Template provisioner script completed for ${VVV_SITE_NAME}"
